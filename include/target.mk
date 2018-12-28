43be15dfb190a01a084621de35c229ba4f2c72ff
hingbong <hingbonglo@gmail.com>
2018-12-28 23:14:49 +0800

    enable luci-app-flowoffloading for lean's feeds

--- include/target.mk
+++ include/target.mk
@@ -15,7 +15,7 @@ DEVICE_TYPE?=router
 # Default packages - the really basic set
 DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd kmod-nf-nathelper kmod-nf-nathelper-extra \
 openssl-util iperf3 ipv6helper \
-luci luci-app-upnp luci-app-ssr-plus \
+luci luci-app-upnp luci-app-ssr-plus luci-app-flowoffload \
 luci-app-vlmcsd
 # For nas targets
 DEFAULT_PACKAGES.nas:=block-mount fdisk lsblk mdadm
